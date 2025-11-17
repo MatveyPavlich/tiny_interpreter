aa227d9a7ef8c5f5e93c3e41d8f40cd870923ea3
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-17 08:09:47 +0000

    feat: test.c program rewritten with fgets()

--- lab/test.c
+++ lab/test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-        int a;
-        printf("Enter your age: ");
-        scanf("%d", &a);
-        printf("Your age is %d\n", a);
+        // int a;
+        // printf("Enter your age: ");
+        // scanf("%d", &a);
+        // printf("Your age is %d\n", a);
+
+        char buffer[100];
+        fputs("Enter your age: ", stdout);
+        fflush(stdout);
+        fgets(buffer, sizeof(buffer), stdin);
+        printf("Your age is %s!\n", buffer);
         return 0;
 }
